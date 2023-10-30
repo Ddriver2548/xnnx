765c39699acba2ff34e35343d0876c43fccf3972
Ddriver2548 <131221254+Ddriver2548@users.noreply.github.com>
2023-10-30 08:58:04 +0700

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://chayanonp:1234@cluster0.ieacyfh.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
